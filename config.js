54a0bda1a217200c07cbee81193b044038b2b09a
Rob Eisenberg <rob@bluespire.com>
2014-12-17 15:21:25 -0500

    fix(package): update dependencies to latest

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ System.config({
 
 System.config({
   "map": {
-    "aurelia-metadata": "github:aurelia/metadata@0.1.0",
+    "aurelia-metadata": "github:aurelia/metadata@0.1.1",
     "es6-shim": "github:paulmillr/es6-shim@0.21.1"
   }
 });
