e0397e6e9228269261bb93e53f5ccebd4d27c68b
Aleksandar Djindjic <djindjic@gmail.com>
2014-12-23 14:40:55 +0100

    test(container): Basic container tests

--- test/container.spec.js
+++ test/container.spec.js
@@ -1,8 +1,83 @@
-import {Container} from '../src/index';
+import {Container, Transient} from '../src/index';
 
 describe('container', () => {
   it('should have some tests', () => {
     var container = new Container();
     expect(container).toBe(container);
+  });
+
+  it('should instantiate a class without injected services', function() {
+    class App {}
+
+    var container = new Container();
+    var app = container.get(App);
+
+    expect(app).toEqual(jasmine.any(App));
+  });
+
+  it('should inject dependencies over static inject function', function() {
+    class Logger {}
+
+    class App {
+      static inject() { return [Logger]; };
+      constructor(logger) {
+        this.logger = logger;
+      }
+    }
+
+    var container = new Container();
+    var app = container.get(App);
+
+    expect(app.logger).toEqual(jasmine.any(Logger));
+  });
+
+  it('inject dependencies in many classes should be singleton', function() {
+    class Logger {}
+
+    class App1 {
+      static inject() { return [Logger]; };
+      constructor(logger) {
+        this.logger = logger;
+      }
+    }
+
+    class App2 {
+      static inject() { return [Logger]; };
+      constructor(logger) {
+        this.logger = logger;
+      }
+    }
+
+    var container = new Container();
+    var app1 = container.get(App1);
+    var app2 = container.get(App2);
+
+    expect(app1.logger).toBe(app2.logger);
+  });
+
+  it('transient inject dependencies in many classes should not be singleton', function() {
+    class Logger {
+      static annotations() { return [new Transient()] };
+    }
+
+    class App1 {
+      static inject() { return [Logger]; };
+      constructor(logger) {
+        this.logger = logger;
+      }
+    }
+
+    class App2 {
+      static inject() { return [Logger]; };
+      constructor(logger) {
+        this.logger = logger;
+      }
+    }
+
+    var container = new Container();
+    var app1 = container.get(App1);
+    var app2 = container.get(App2);
+
+    expect(app1.logger).not.toBe(app2.logger);
   });
 });
