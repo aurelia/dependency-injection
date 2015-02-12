6c5ee91fc9d0c155adba0fcfe53ed14650a3c72e
Rob Eisenberg <rob@bluespire.com>
2015-02-12 09:40:14 -0500

    chore(build): add build args for version bump

--- build/tasks/prepare-release.js
+++ build/tasks/prepare-release.js
@@ -4,10 +4,11 @@ var paths = require('../paths');
 var changelog = require('conventional-changelog');
 var fs = require('fs');
 var bump = require('gulp-bump');
+var args = require('../args');
 
 gulp.task('bump-version', function(){
   return gulp.src(['./package.json', './bower.json'])
-    .pipe(bump({type:'patch'})) //major|minor|patch|prerelease
+    .pipe(bump({type:args.bump })) //major|minor|patch|prerelease
     .pipe(gulp.dest('./'));
 });
 
