0cdfdb0afd0788123f333518ee11ffdb8d192288
Rob Eisenberg <rob@bluespire.com>
2015-07-29 16:15:39 -0400

    chore(build): fix module output file name

--- config.js
+++ config.js
@@ -17,12 +17,12 @@ System.config({
 
 System.config({
   "map": {
-    "aurelia-logging": "github:aurelia/logging@0.6.0",
-    "aurelia-metadata": "github:aurelia/metadata@0.7.0",
+    "aurelia-logging": "github:aurelia/logging@0.6.2",
+    "aurelia-metadata": "github:aurelia/metadata@0.7.1",
     "babel": "npm:babel-core@5.1.13",
     "babel-runtime": "npm:babel-runtime@5.1.13",
     "core-js": "npm:core-js@0.9.18",
-    "github:aurelia/metadata@0.7.0": {
+    "github:aurelia/metadata@0.7.1": {
       "core-js": "npm:core-js@0.9.18"
     },
     "github:jspm/nodelibs-process@0.1.1": {
