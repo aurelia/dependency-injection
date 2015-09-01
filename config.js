97d032c85c747e25eb1bcd87d46f211916d2f239
Rob Eisenberg <rob@bluespire.com>
2015-09-01 00:53:26 -0400

    fix(build): update linting, testing and tools

--- config.js
+++ config.js
@@ -1,30 +1,25 @@
 System.config({
-  "transpiler": "babel",
-  "babelOptions": {
+  defaultJSExtensions: true,
+  transpiler: "babel",
+  babelOptions: {
     "optional": [
       "runtime",
       "es7.decorators",
       "es7.classProperties"
     ]
   },
-  "paths": {
-    "*": "*.js",
-    "github:*": "jspm_packages/github/*.js",
-    "aurelia-dependency-injection/*": "dist/*.js",
-    "npm:*": "jspm_packages/npm/*.js"
-  }
-});
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "aurelia-dependency-injection/*": "dist/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
 
-System.config({
-  "map": {
+  map: {
     "aurelia-logging": "github:aurelia/logging@0.6.3",
     "aurelia-metadata": "github:aurelia/metadata@0.7.3",
     "babel": "npm:babel-core@5.1.13",
     "babel-runtime": "npm:babel-runtime@5.1.13",
-    "core-js": "npm:core-js@0.9.18",
-    "github:aurelia/metadata@0.7.1": {
-      "core-js": "npm:core-js@0.9.18"
-    },
+    "core-js": "npm:core-js@1.1.3",
     "github:aurelia/metadata@0.7.3": {
       "core-js": "npm:core-js@0.9.18"
     },
@@ -35,7 +30,11 @@ System.config({
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:core-js@1.1.3": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     }
   }
 });
-
