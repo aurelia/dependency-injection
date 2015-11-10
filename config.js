763a571dca258d6e25c25180b4cd9c4b605b010c
Rob Eisenberg <rob@bluespire.com>
2015-11-09 23:12:38 -0500

    chore(package): update dependencies

--- config.js
+++ config.js
@@ -15,18 +15,21 @@ System.config({
   },
 
   map: {
-    "aurelia-logging": "github:aurelia/logging@0.8.0",
-    "aurelia-metadata": "github:aurelia/metadata@0.9.0",
-    "aurelia-pal": "github:aurelia/pal@0.2.0",
+    "aurelia-logging": "github:aurelia/logging@0.9.0",
+    "aurelia-metadata": "github:aurelia/metadata@0.10.0",
+    "aurelia-pal": "github:aurelia/pal@0.3.0",
     "babel": "npm:babel-core@5.8.25",
     "babel-runtime": "npm:babel-runtime@5.8.25",
-    "core-js": "npm:core-js@0.9.18",
-    "github:aurelia/metadata@0.9.0": {
-      "aurelia-pal": "github:aurelia/pal@0.2.0",
-      "core-js": "npm:core-js@0.9.18"
+    "core-js": "npm:core-js@1.2.6",
+    "github:aurelia/metadata@0.10.0": {
+      "aurelia-pal": "github:aurelia/pal@0.3.0",
+      "core-js": "npm:core-js@1.2.6"
     },
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
+    },
+    "github:jspm/nodelibs-path@0.1.0": {
+      "path-browserify": "npm:path-browserify@0.0.0"
     },
     "github:jspm/nodelibs-process@0.1.2": {
       "process": "npm:process@0.11.2"
@@ -40,13 +43,17 @@ System.config({
     "npm:babel-runtime@5.8.25": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:core-js@0.9.18": {
+    "npm:core-js@1.2.6": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.2",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:path-browserify@0.0.0": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:process@0.11.2": {
       "assert": "github:jspm/nodelibs-assert@0.1.0"
