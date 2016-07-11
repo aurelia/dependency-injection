83a1a9241536dd486a5c25e1d17c377b3dcd5f9e
David Driscoll <david.driscoll@gmail.com>
2016-07-11 09:53:19 -0400

    feat(resolver): added decorators for lazy, all, optional, parent, factory and newInstance

--- src/injection.js
+++ src/injection.js
@@ -17,6 +17,19 @@ export function autoinject(potentialTarget?: any): any {
 */
 export function inject(...rest: any[]): any {
   return function(target, key, descriptor) {
+    // handle when used as a parameter
+    if (typeof descriptor === 'number' && rest.length === 1) {
+      let params = target.inject;
+      if (typeof params === 'function') {
+        throw new Error('Decorator inject cannot be used with "inject()".  Please use an array instead.');
+      }
+      if (!params) {
+        params = metadata.getOwn(metadata.paramTypes, target).concat();
+        target.inject = params;
+      }
+      params[descriptor] = rest[0];
+      return;
+    }
     // if it's true then we injecting rest into function and not Class constructor
     if (descriptor) {
       const fn = descriptor.value;
