24e19a21617527230c84a1a712229771265262ab
David Driscoll <david.driscoll@gmail.com>
2016-07-11 09:53:19 -0400

    feat(resolver): added decorators for lazy, all, optional, parent, factory and newInstance

--- src/resolvers.js
+++ src/resolvers.js
@@ -300,3 +300,88 @@ export class NewInstance {
     return new NewInstance(key);
   }
 }
+
+function getDeocratorDependencies(target, name) {
+  let dependencies = target.inject;
+  if (typeof dependencies === 'function') {
+    throw new Error('Decorator ' + name + ' cannot be used with "inject()".  Please use an array instead.');
+  }
+  if (!dependencies) {
+    dependencies = metadata.getOwn(metadata.paramTypes, target).concat();
+    target.inject = dependencies;
+  }
+  return dependencies;
+}
+
+/**
+* Decorator: Specifies the dependency should be lazy loaded
+*/
+export function lazy(keyValue: any) {
+  return function(target, key, index) {
+    let params = getDeocratorDependencies(target, 'lazy');
+    params[index] = Lazy.of(keyValue);
+  };
+}
+
+/**
+* Decorator: Specifies the dependency should load all instances of the given key.
+*/
+export function all(keyValue: any) {
+  return function(target, key, index) {
+    let params = getDeocratorDependencies(target, 'all');
+    params[index] = All.of(keyValue);
+  };
+}
+
+/**
+* Decorator: Specifies the dependency as optional
+*/
+export function optional(checkParentOrTarget: boolean = true) {
+  let deco = function(checkParent: boolean) {
+    return function(target, key, index) {
+      let params = getDeocratorDependencies(target, 'optional');
+      params[index] = Optional.of(params[index], checkParent);
+    };
+  };
+  if (typeof checkParentOrTarget === 'boolean') {
+    return deco(checkParentOrTarget);
+  }
+  return deco(true);
+}
+
+/**
+* Decorator: Specifies the dependency to look at the parent container for resolution
+*/
+export function parent(target, key, index) {
+  let params = getDeocratorDependencies(target, 'parent');
+  params[index] = Parent.of(params[index]);
+}
+
+/**
+* Decorator: Specifies the dependency to create a factory method, that can accept optional arguments
+*/
+export function factory(keyValue: any) {
+  return function(target, key, index) {
+    let params = getDeocratorDependencies(target, 'factory');
+    params[index] = Factory.of(keyValue);
+  };
+}
+
+/**
+* Decorator: Specifies the dependency as a new instance
+*/
+export function newInstance(asKeyOrTarget?: any) {
+  let deco = function(asKey?: any) {
+    return function(target, key, index) {
+      let params = getDeocratorDependencies(target, 'newInstance');
+      params[index] = NewInstance.of(params[index]);
+      if (!!asKey) {
+        params[index].as(asKey);
+      }
+    };
+  };
+  if (arguments.length === 1) {
+    return deco(asKeyOrTarget);
+  }
+  return deco();
+}
